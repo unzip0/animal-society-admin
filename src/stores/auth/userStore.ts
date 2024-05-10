b63ae3df0da8cc63153fdb22c00f5af836bd5c06
acabrera <acabrera@t4xi.com>
2024-05-10 15:12:43 +0200

    [TASK] user state store

---
+++ src/stores/auth/userStore.ts
@@ -0,0 +1,36 @@
+import { defineStore } from "pinia";
+
+export const userStore = defineStore("user", {
+    state: () => ({
+        storedToken: localStorage.getItem("token") || null,
+        storedUser: localStorage.getItem("user") || null
+    }),
+    getters: {
+        user: state => {
+            if (!!state.storedUser) {
+                return JSON.parse(state.storedUser);
+            }
+            return state.storedUser;
+        },
+        token: state => {
+            if (!!state.storedToken) {
+                return JSON.parse(state.storedToken);
+            }
+            return state.storedToken;
+        },
+        userIsAuth: state => !!state.storedToken
+    },
+    actions: {
+        storeLoggedInUser(token, user) {
+            const _this = this;
+
+            const stringifiedToken = JSON.stringify(token);
+            localStorage.setItem('token', stringifiedToken);
+            const stringifiedUser = JSON.stringify(user);
+            localStorage.setItem('user', stringifiedUser);
+
+            _this.storedToken = stringifiedToken;
+            _this.storedUser = stringifiedUser;
+        }
+    }
+});
