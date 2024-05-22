3e5f8fe54b9e8ecb4b29c8c69484f8bf774c2d9c
acabrera <acabrera@t4xi.com>
2024-05-22 23:39:49 +0200

    [TASK] axios interceptor for set auth header and logout when unauthorized

---
+++ src/infrastructure/axios/axios.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import { userStore } from '../../stores/auth/userStore';
+import router from '../../router/router';
+
+
+const axiosInstance = axios.create({
+  baseURL: `${import.meta.env.VITE_API_URL}`,
+});
+
+const authStore = userStore();
+
+axiosInstance.interceptors.request.use((config) => {
+  
+  if (authStore.token) {
+    config.headers.Authorization = `${authStore.token.type} ${authStore.token.token}`;
+  }
+  
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      authStore.logout();
+      router.push({ name: 'Authentication' });
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
