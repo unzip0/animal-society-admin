1de9b537812d9cbf9c0fb28fd36a4d638503a31d
acabrera <acabrera@t4xi.com>
2024-06-05 16:36:29 +0200

    [TASK] formatting in/out requests

--- src/core/shared/infrastructure/axios/routes/HttpUserRouting.ts
+++ src/core/shared/infrastructure/axios/routes/HttpUserRouting.ts
@@ -1,6 +1,11 @@
+import { ApiResponse } from '../../response/ApiResponse';
 import axios from '../axios';
 import HttpRoutes from './HttpRoutes';
 
-export const updateUser = (id: string, data: { name: string; first_last_name: string; second_last_name: string }) => {
+export const update = (id: string, data: { 
+  name: string,
+  first_last_name: string,
+  second_last_name: string
+}): Promise<ApiResponse> => {
   return axios.patch(HttpRoutes.users.update(id), data);
 };
